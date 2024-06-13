26d01ae028a0c22a7218f013f5a4bcc0f06a2d73
Anthony Frassino <tonyfrin@gmail.com>
2024-06-12 20:15:53 -0400

    Version 0.1.85 add Pixel Faceboock

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from 'next/head';
 import { ThemeProvider } from "../Abstract/context/ThemeContext";
+import FacebookPixel from "../Abstract/Component/FacebookPixel";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
         </Head>
+        <FacebookPixel />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
