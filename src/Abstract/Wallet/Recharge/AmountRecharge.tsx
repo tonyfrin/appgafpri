32b9765891c3577412c69677cc3d36a932a2fd2e
Anthony Frassino <tonyfrin@gmail.com>
2024-08-06 03:12:58 -0400

    Version 0.1.123 out paypal

--- src/Abstract/Wallet/Recharge/AmountRecharge.tsx
+++ src/Abstract/Wallet/Recharge/AmountRecharge.tsx
@@ -121,8 +121,6 @@ export function AmountRecharge() {
   const [mainCurrencyIsReady, setMainCurrencyIsReady] = React.useState(false);
   const pagoMovilCurrencyId = '2';
 
-  
-
 
 
   const next = () => {
@@ -358,7 +356,7 @@ export function AmountRecharge() {
                   }}>
                     <span className={textInfoStyles}>Los datos para transferencia Zelle son:</span>
                     <span className={textInfoStyles}>A nombre de: <span style={{fontWeight: 600}}>Gafpri Corp</span></span>
-                    <span className={textInfoStyles}>Correo: <span style={{fontWeight: 600}}>billetera@gafpri.com</span></span>
+                    <span className={textInfoStyles}>Correo: <span style={{fontWeight: 600}}>pagos@gafpri.com</span></span>
                   </div>
                 </div>
               }
